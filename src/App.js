44c0c59256161775f80f6e74b9cd24c711414657
Joe Domovitch <jdomovitch@gmail.com>
2019-10-05 14:15:55 -0700

    render component test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Header/>
     </div>
   );
 }
