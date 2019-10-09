5bc0806f4d4cb586750e061af2eb286928f1d4ab
Joe Domovitch <jdomovitch@gmail.com>
2019-10-09 13:01:05 -0700

    remove header/navbar transparency

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.scss';
-import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Header from './components/Header'
+import About from './components/About'
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <About/>
     </div>
   );
 }
