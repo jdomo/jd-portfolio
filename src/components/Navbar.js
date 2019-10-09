d9e838bab9bf45f6fb405fdb9dd7986a9ce71ed2
Joe Domovitch <jdomovitch@gmail.com>
2019-10-08 20:15:30 -0700

    navbar markup

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar__linklist">
+        <li className="navbar__link">Skills</li>
+        <li className="navbar__link">Projects</li>
+        <li className="navbar__link">Experience</li>
+        <li className="navbar__link">Education</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
