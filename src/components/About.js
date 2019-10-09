f969b1b8ae002afc3dfb37f1637ec0b260e77362
Joe Domovitch <jdomovitch@gmail.com>
2019-10-09 16:31:13 -0700

    insert image and text for about section - need to get text to wrap

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,22 @@ const About = () => {
   return (
     <section className="section-about">
       <div className="u-center-text">
-        <h3 className="heading-secondary" id="about">About Me.</h3>
+        <h3 className="heading-secondary" id="about">About Me</h3>
+      </div>
+      <div className="u-margin-top">
+        <div className="row">
+          <figure className="section-about__shape">
+            <img src="img/jd.jpg" className="section-about__img" alt="Joseph"/>
+          </figure>
+          <div className="section-about__text">
+            <p>
+              I am a software engineer with a colourful background in music production and psychology. 
+              I’m passionate about programming, music, food, and travel. Striving to deliver creative yet 
+              straightforward solutions, I am seeking a position where I can apply my skills while tackling 
+              any challenge presented.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   )
