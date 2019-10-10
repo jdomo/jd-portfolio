7b9ce1f1581a2d9af17d177b0612f514e3081f6e
Joe Domovitch <jdomovitch@gmail.com>
2019-10-09 18:56:09 -0700

    apply fixed navbar styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <section className="section-about">
-      <div className="u-center-text">
+      <div className="u-center-text u-margin-bottom-med">
         <h3 className="heading-secondary" id="about">About Me</h3>
       </div>
       <div className="u-margin-top">
