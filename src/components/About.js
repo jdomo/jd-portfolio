309ff925cb49b676a36e5bb8b3bb0542c90a3949
Joe Domovitch <jdomovitch@gmail.com>
2019-10-09 17:25:06 -0700

    wrap text around image

--- src/components/About.js
+++ src/components/About.js
@@ -7,17 +7,19 @@ const About = () => {
         <h3 className="heading-secondary" id="about">About Me</h3>
       </div>
       <div className="u-margin-top">
-        <div className="row">
-          <figure className="section-about__shape">
-            <img src="img/jd.jpg" className="section-about__img" alt="Joseph"/>
-          </figure>
-          <div className="section-about__text">
-            <p>
-              I am a software engineer with a colourful background in music production and psychology. 
-              I’m passionate about programming, music, food, and travel. Striving to deliver creative yet 
-              straightforward solutions, I am seeking a position where I can apply my skills while tackling 
-              any challenge presented.
-            </p>
+        <div className="section-about__content">
+          <div className="row">
+            <figure className="section-about__shape">
+              <img src="img/jd.jpg" className="section-about__img" alt="Joseph"/>
+            </figure>
+            <div className="section-about__text">
+              <p>
+                I am a software engineer with a colourful background in music production and psychology. 
+                I’m passionate about programming, music, food, and travel. Striving to deliver creative yet 
+                straightforward solutions, I am seeking a position where I can apply my skills while tackling 
+                any challenge presented.
+              </p>
+            </div>
           </div>
         </div>
       </div>
