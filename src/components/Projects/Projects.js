50477aeeb5f500e6a244654b829a0b701411f96b
Joe Domovitch <jdomovitch@gmail.com>
2019-10-13 11:20:03 -0700

    begin work on project components

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,13 +4,16 @@ class Projects extends Component {
   render() {
     return (
       <div className="section-projects">
+        <div className="u-center-text u-margin-bottom-med">
+          <h3 className="heading-secondary" id="about">Projects</h3>
+        </div>
         <div className="row">
           <div className="col-1-of-2">
             <h3 className="heading-tertiary">How Many Words</h3>
             <img src="img/how-many-words.png" alt="How Many Words" className="section-projects__img"></img>
           </div>
           <div className="col-1-of-2">
-
+          How Many Words can you make out of 9 random letters? Set your high score, then step your game up and try to top it. You've got 120 seconds.
           </div>
         </div>
         <div className="row">
