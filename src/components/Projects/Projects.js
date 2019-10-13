d86d81ea4c9e721169f79158a889b67bc8d336c5
Joe Domovitch <jdomovitch@gmail.com>
2019-10-12 18:46:10 -0700

    basic markup for projects section - need to add class for section-projects__img

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,31 @@ class Projects extends Component {
   render() {
     return (
       <div className="section-projects">
-        Projects
+        <div className="row">
+          <div className="col-1-of-2">
+            <h3 className="heading-tertiary">How Many Words</h3>
+            <img src="img/how-many-words.png" alt="How Many Words" className="section-projects__img"></img>
+          </div>
+          <div className="col-1-of-2">
+
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-1-of-2">
+            
+          </div>
+          <div className="col-1-of-2">
+
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-1-of-2">
+            
+          </div>
+          <div className="col-1-of-2">
+
+          </div>
+        </div>
       </div>
     )
   }
