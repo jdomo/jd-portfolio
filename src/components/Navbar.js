f84c7a1288f60839966e81e8f836e7f50495ced8
Joe Domovitch <jdomovitch@gmail.com>
2019-10-23 20:53:38 -0700

    responsive nav - still need to figure out disappear transition

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,45 @@
-import React from "react";
+import React, {Component} from "react";
+import $ from "jquery";
 
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <ul className="navbar__linklist">
-        <button className="navbar__button">❖</button>
-        <li className="navbar__item">
-          <a href="#about" className="navbar__link">About Me</a>
-        </li>
-        <li className="navbar__item">
-          <a href="#skills" className="navbar__link">Skills</a>
-        </li>
-        <li className="navbar__item">
-          <a href="#projects" className="navbar__link">Projects</a>
-        </li>
-        <li className="navbar__item">
-          <a href="/docs/jdomovitchresume.pdf" className="navbar__link" target="_blank" rel="noopener noreferrer">Resume</a>
-        </li>
-      </ul>
-    </nav>
-  )
+class Navbar extends Component {
+  state = {
+    active: false,
+  }
+
+  toggleClass = () => {
+    const currState = this.state.active;
+    this.setState({
+      active: !currState
+    });
+  }
+
+  render() {
+    console.log(this.state, '<---state');
+    return (
+      <nav className={this.state.active ? 'navbar' : 'navbar navbar--hide'}>
+        <div className="u-center-text">
+          <button 
+            className={this.state.active ? 'navbar__button navbar__button--clicked' : 'navbar__button'}
+            onClick={this.toggleClass}
+          >❖</button>
+        </div>
+        <ul className="navbar__linklist">
+          <li className={this.state.active ? 'navbar__item' : 'navbar__item navbar__item--hide'}>
+            <a href="#about" className="navbar__link" onClick={this.toggleClass}>About Me</a>
+          </li>
+          <li className={this.state.active ? 'navbar__item' : 'navbar__item navbar__item--hide'}>
+            <a href="#skills" className="navbar__link" onClick={this.toggleClass}>Skills</a>
+          </li>
+          <li className={this.state.active ? 'navbar__item' : 'navbar__item navbar__item--hide'}>
+            <a href="#projects" className="navbar__link" onClick={this.toggleClass}>Projects</a>
+          </li>
+          <li className={this.state.active ? 'navbar__item' : 'navbar__item navbar__item--hide'}>
+            <a href="/docs/jdomovitchresume.pdf" className="navbar__link" target="_blank" rel="noopener noreferrer" onClick={this.toggleClass}>Resume</a>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
 }
 
 export default Navbar;
