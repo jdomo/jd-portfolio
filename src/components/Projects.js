7f3df62f13e31c8a888af93ea1a6c065f0625b8f
Joe Domovitch <jdomovitch@gmail.com>
2019-10-28 16:26:29 -0700

    change placement of project links on mobile

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,10 +20,6 @@ class Projects extends Component {
             </div>
             <div className="col-1-of-2">
               <h3 className="heading-tertiary u-margin-bottom-sml">How Many Words</h3>
-              <div className="project__links--mobile">
-                <a href="https://pages.git.generalassemb.ly/jdomo/how-many-words/" className="project__link--mobile" target="_blank" rel="noopener noreferrer">Website</a>
-                <a href="https://github.com/jdomo/how-many-words" className="project__link--mobile" target="_blank" rel="noopener noreferrer">Code</a>
-              </div>
               <p className="project__text">
                 How Many Words can you make out of 9 random letters? Set your high score, then step your game up and try to top it. You've got 120 seconds.
                 All you need is a web browser, a keyboard and your smarty pants.
@@ -31,6 +27,10 @@ class Projects extends Component {
               <h4 className="heading-tertiary--sub u-margin-top">
                 Built with HTML, CSS, JavaScript, GitHub
               </h4>
+              <div className="project__links--mobile">
+                <a href="https://pages.git.generalassemb.ly/jdomo/how-many-words/" className="project__link--mobile" target="_blank" rel="noopener noreferrer">Website</a>
+                <a href="https://github.com/jdomo/how-many-words" className="project__link--mobile" target="_blank" rel="noopener noreferrer">Code</a>
+              </div>
               <p className="project__icons u-margin-top-sml">
                 <i className="devicon-html5-plain project__icon"></i>
                 <i className="devicon-css3-plain project__icon"></i>
@@ -45,10 +45,6 @@ class Projects extends Component {
           <div className="row">
             <div className="col-1-of-2">
               <h3 className="heading-tertiary u-margin-bottom-sml">Watch Your Tone</h3>
-              <div className="project__links--mobile">
-                <a href="https://watch-your-tone.herokuapp.com/" className="project__link--mobile" target="_blank" rel="noopener noreferrer">Website</a>
-                <a href="http://www.github.com" className="project__link--mobile" target="_blank" rel="noopener noreferrer">Code</a>
-              </div>
               <p className="project__text">
               Ever wonder about how an AI interprets the things we say? Using IBM's Tone Analyzer, 
               Watch Your Tone enlists the processing power of Watson to determine the emotional content of your favourite phrases and quotes.
@@ -56,6 +52,10 @@ class Projects extends Component {
               <h4 className="heading-tertiary--sub u-margin-top">
                 Built with HTML, CSS, JavaScript, React, Express.js, IBM Watson & Tone Analyzer, NodeJS, MongoDB, Mongoose, Materialize, Chart.js, Github
               </h4>
+              <div className="project__links--mobile">
+                <a href="https://watch-your-tone.herokuapp.com/" className="project__link--mobile" target="_blank" rel="noopener noreferrer">Website</a>
+                <a href="http://www.github.com" className="project__link--mobile" target="_blank" rel="noopener noreferrer">Code</a>
+              </div>
               <p className="project__icons u-margin-top-sml">
                 <i className="devicon-html5-plain project__icon"></i>
                 <i className="devicon-css3-plain project__icon"></i>
@@ -92,10 +92,6 @@ class Projects extends Component {
             </div>
             <div className="col-1-of-2">
               <h3 className="heading-tertiary u-margin-bottom-sml">Best Dish</h3>
-              <div className="project__links--mobile">
-                <a href="https://sei-bestdish.herokuapp.com/" className="project__link--mobile" target="_blank" rel="noopener noreferrer">Website</a>
-                <a href="https://github.com/jdomo/best-dish" className="project__link--mobile" target="_blank" rel="noopener noreferrer">Code</a>
-              </div>
               <p className="project__text">
                 Food blog with CRUD functionality. Many food websites focus mainly on the restaurant - 
                 Best Dish is all about the star of the show, the food. Sign up today and share your best dish!
@@ -103,6 +99,10 @@ class Projects extends Component {
               <h4 className="heading-tertiary--sub u-margin-top">
                 Built with HTML, CSS, JavaScript, Express.js, NodeJS, MongoDB, Mongoose, Github
               </h4>
+              <div className="project__links--mobile">
+                <a href="https://sei-bestdish.herokuapp.com/" className="project__link--mobile" target="_blank" rel="noopener noreferrer">Website</a>
+                <a href="https://github.com/jdomo/best-dish" className="project__link--mobile" target="_blank" rel="noopener noreferrer">Code</a>
+              </div>
               <p className="project__icons u-margin-top-sml">
                 <i className="devicon-html5-plain project__icon"></i>
                 <i className="devicon-css3-plain project__icon"></i>
