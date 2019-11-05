93f2eb5ebfc6a5370f49d9ab878a226f1c1e5d55
Joe Domovitch <jdomovitch@gmail.com>
2019-11-05 10:22:24 -0800

    tweak positioning of skills icons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About/>
       <Skills/>
       <Projects/>
+      <Footer/>
     </div>
   );
 }
