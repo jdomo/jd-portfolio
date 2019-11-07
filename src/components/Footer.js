97c292779420f0c135b9b74b266a88e6e350a6c4
Joe Domovitch <jdomovitch@gmail.com>
2019-11-07 14:30:51 -0800

    add edited resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer" id="contact">
       <div className="footer__content">
         <h3 className="heading-tertiary footer__heading">Contact Me:</h3>
-        <h3 className="heading-tertiary--sub footer__heading footer__heading--sub">jdomovitch@gmail.com</h3>
+        <h3 className="heading-tertiary--sub footer__heading footer__heading--sub">domodevLA@gmail.com</h3>
         <div className="footer__links">
           <a className="footer__link" href="https://www.linkedin.com/in/josephdomovitch" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-linkedin" id="linked-in"></i>
