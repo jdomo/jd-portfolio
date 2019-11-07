716d534191df353aaa97478a52b5703e8f0aab4e
Joe Domovitch <jdomovitch@gmail.com>
2019-11-07 12:09:08 -0800

    Updates

---
+++ precache-manifest.996b2f2d04a1d8eef42a21db43736d54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d219b626c3e287622e6414a715b6cfb0",
+    "url": "/jd-portfolio/index.html"
+  },
+  {
+    "revision": "e9d624e499c85c33e386",
+    "url": "/jd-portfolio/static/css/main.062852d8.chunk.css"
+  },
+  {
+    "revision": "9fdc7a1046661b837412",
+    "url": "/jd-portfolio/static/js/2.8b34e3f7.chunk.js"
+  },
+  {
+    "revision": "e9d624e499c85c33e386",
+    "url": "/jd-portfolio/static/js/main.37627b71.chunk.js"
+  },
+  {
+    "revision": "5c45a356befb73d3f1bd",
+    "url": "/jd-portfolio/static/js/runtime-main.8392b893.js"
+  }
+]);
