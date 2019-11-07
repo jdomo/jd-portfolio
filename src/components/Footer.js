4a92492d30bac160023f426d38483da34aa55815
Joe Domovitch <jdomovitch@gmail.com>
2019-11-07 11:17:46 -0800

    add footer content and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      bla bla
+      <div className="footer__content">
+        <h3 className="heading-tertiary footer__heading">Contact Me:</h3>
+        <h3 className="heading-tertiary--sub footer__heading footer__heading--sub">jdomovitch@gmail.com</h3>
+        <div className="footer__links">
+          <a className="footer__link" href="https://www.linkedin.com/in/josephdomovitch">
+            <i className="fab fa-linkedin" id="linked-in"></i>
+          </a>
+          <a className="footer__link" href="https://www.github.com/jdomo">
+            <i className="devicon-github-plain" id="github"></i>
+          </a>
+        </div>
+      </div>
     </footer>
   )
 }
